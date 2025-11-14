12e1bdb089d5c5e22928164d4cd4d5b9ca3daeec
xkarina2197 <xkarina2197@gmail.com>
2025-11-14 14:55:03 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+# 8-misol
+matn = "  java vs python  "
+print(matn.title())
+print(matn.rstrip())
+
+# 9-misol
+matn = "oquvchi talaba"
+print(matn.capitalize())
+print(len(matn))
+
+# 10-misol
+matn = "  DATA SCIENCE  "
+print(matn.strip())
+print(matn.upper())
+print(matn.lower())
+
+# 11-misol
+matn = "web dasturchi"
+print(matn.title())
+print(matn.lstrip())
+
+
+# 12-misol
+matn = "MACHINE LEARNING"
+print(matn.rstrip())
+print(matn.capitalize())
+
+# 13-misol
+matn = "python java c++"
+print(matn.upper())
+print(len(matn))
+
+# 14-misol
+matn = "  arfifical intelligence  "
+print(matn.strip())
+print(matn.title())
+
+# 15-misol
+matn = "SQL VA NoSQL"
+print(matn.lower())
+print(matn.capitalize())
+print(len(matn))
